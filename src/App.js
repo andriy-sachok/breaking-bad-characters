f8ccee8815f7e9e0af95ef05f86e76f5024d9bab
Andriy <andriy.sachok@gmail.com>
2022-12-30 22:39:53 +0200

    added custom color palette

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import { Routes, Route, useLocation } from "react-router-dom";
 
 import { Box, Container } from "@mui/material";
+import { createTheme, ThemeProvider } from "@mui/material";
+import { grey, blueGrey, cyan } from "@mui/material/colors";
 
 import { AnimatePresence } from "framer-motion";
 import AnimatedComponent from "./components/animations/AnimatedComponent";
@@ -11,34 +13,53 @@ import Gallery from "./components/Gallery";
 import CharacterInfo from "./components/character/CharacterInfo";
 import DummyComponent from "./components/animations/DummyComponent";
 
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: grey[900],
+    },
+    secondary: {
+      main: blueGrey[100],
+    },
+    // backgroundColor: {
+    //   header:
+    // },
+    // text: {},
+  },
+});
+
 function App() {
   const location = useLocation();
   return (
-    <Container maxWidth="false" disableGutters>
-      <Header />
-      <Box
-        sx={{
-          display: `flex`,
-          // padding: `1rem`,
-        }}
+    <ThemeProvider theme={theme}>
+      <Container
+        maxWidth="false"
+        disableGutters
+        sx={{ backgroundColor: `secondary.main` }}
       >
-        <AnimatePresence mode={`wait`}>
-          <Routes key={location.pathname} location={location}>
-            <Route path={"/"} exact element={<Gallery />} />
-            <Route
-              path={"/:id"}
-              element={
-                <AnimatedComponent>
-                  <CharacterInfo />
-                </AnimatedComponent>
-              }
-            />
-            <Route path="dummy" element={<DummyComponent />} />
-          </Routes>
-        </AnimatePresence>
-      </Box>
-      {/* <Gallery /> */}
-    </Container>
+        <Header />
+        <Box
+          sx={{
+            display: `flex`,
+          }}
+        >
+          <AnimatePresence mode={`wait`}>
+            <Routes key={location.pathname} location={location}>
+              <Route path={"/"} exact element={<Gallery />} />
+              <Route
+                path={"/:id"}
+                element={
+                  <AnimatedComponent>
+                    <CharacterInfo />
+                  </AnimatedComponent>
+                }
+              />
+              <Route path="dummy" element={<DummyComponent />} />
+            </Routes>
+          </AnimatePresence>
+        </Box>
+      </Container>
+    </ThemeProvider>
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -4,8 +4,8 @@ import Typography from "@mui/material/Typography";
 
 const Header = () => {
   return (
-    <AppBar position="static" sx={{ backgroundColor: `grey` }}>
-      <Typography variant="h4" textAlign={"center"} margin="0.5rem">
+    <AppBar position="static" sx={{ backgroundColor: `primary.main` }}>
+      <Typography variant="h4" textAlign="center" margin="0.5rem">
         Breaking Bad characters
       </Typography>
     </AppBar>
